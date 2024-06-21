39064318f923511527ce5f99a43fbdcab93195dd
anuratan1421 <anuratanbahadure@gmail.com>
2024-06-22 02:03:30 +0530

    gpt

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.set('views', './views');
+
 app.set('view engine', 'ejs');
 
 /* Write your code here:
@@ -20,7 +20,7 @@ Step 4: Add the partials to the about and contact pages to show the header and f
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
